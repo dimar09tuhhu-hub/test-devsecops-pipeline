83124d0ffcd5981b3cd2259ac63cf34b0a4530fe
Dimar Alam <dimar09tuhhu@gmail.com>
2026-07-08 15:06:13 +0700

    uji coba trigger workflow ke-21

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 
 app.get('/run', (req, res) => {
     const code = req.query.code;
-    
+
     // 🔴 SANGAT BERBAHAYA: Menggunakan eval() untuk menjalankan input dari query string secara langsung.
     // Ini menimbulkan celah keamanan Remote Code Execution (RCE) yang akan dideteksi oleh Semgrep.
-    const result = eval(code); 
-    
+    const result = eval(code);
+
     res.send(`Hasil evaluasi: ${result}`);
 });
 
